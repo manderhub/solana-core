6d4514afa7462ed1ae0d78a3f1474fcf3cae6f9d
mario <mander.and@gmail.com>
2022-11-19 00:18:48 +0100

    initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,39 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [executable, setExecutable] = useState(false)
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+    try {
+      setAddress(address);
+      const key = new web3.PublicKey(address);
+      // convert to base-58 representation of public key
+      setAddress(key.toBase58());
+      // setup connection to devnet
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
+      // query for balance of account with given key
+      connection.getBalance(key).then(balance => {
+        // convert lamports to sol
+        setBalance(balance / web3.LAMPORTS_PER_SOL);
+      })
+      connection.getAccountInfo(key).then(accountInfo => {
+        if (accountInfo?.executable) {
+          setExecutable(true);
+        } else {
+          setExecutable(false);
+        }
+      })
+    } catch (error) {
+      setAddress('');
+      setBalance(0);
+      alert(error);
+    }
+    
   }
 
   return (
@@ -23,6 +48,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${executable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
   )
